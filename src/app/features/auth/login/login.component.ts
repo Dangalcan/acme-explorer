8e95e21b20f9cd022bca1aa4483c5a4b86d535de
sergioapi <seralpi99@gmail.com>
2026-03-12 18:13:28 +0100

    feat: generated auth-panel files and renamed login folder

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
 import { AuthService } from '../../../core/services/auth.service';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,19 +10,22 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-
   private authService = inject(AuthService);
+  private router = inject(Router);
 
-  email = signal('')
-  password = signal('')
+  email = signal('');
+  password = signal('');
+  errorMessage = signal('');
 
   async login() {
+    this.errorMessage.set('');
+
     try {
       await this.authService.login(this.email(), this.password());
-      const user = this.authService.currentUser();
-      console.log("Login successfully made! Hi " + user?.email);
+      await this.router.navigateByUrl('/');
     } catch (err) {
       console.error(err);
+      this.errorMessage.set('Invalid email or password');
     }
   }
 }
---
+++ src/app/shared/auth-panel/auth-panel.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-panel',
+  imports: [],
+  templateUrl: './auth-panel.html',
+  styleUrl: './auth-panel.scss',
+})
+export class AuthPanel {}
