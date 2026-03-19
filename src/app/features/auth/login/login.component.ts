489680334638dcb26306b2d966c6119fed8c8400
dangalcan <megamagolas@gmail.com>
2026-03-19 11:45:47 +0100

    style: add styles and animations to login page

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,11 +1,11 @@
 import { Component, effect, inject, signal } from '@angular/core';
 import { AuthService } from '../../../core/services/auth.service';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
@@ -16,6 +16,8 @@ export class LoginComponent {
   email = signal('');
   password = signal('');
   errorMessage = signal('');
+  isShaking = signal(false);
+  isLoading = signal(false);
 
   constructor() {
     effect(() => {
@@ -27,6 +29,7 @@ export class LoginComponent {
 
   async login() {
     this.errorMessage.set('');
+    this.isLoading.set(true);
 
     try {
       await this.authService.login(this.email(), this.password());
@@ -34,10 +37,18 @@ export class LoginComponent {
     } catch (err) {
       console.error(err);
       this.errorMessage.set('Invalid email or password');
+      this.triggerShake();
+    } finally {
+      this.isLoading.set(false);
     }
   }
 
   goBack() {
     this.router.navigateByUrl('/');
+  }
+
+  private triggerShake() {
+    this.isShaking.set(false);
+    setTimeout(() => this.isShaking.set(true), 10);
   }
 }
