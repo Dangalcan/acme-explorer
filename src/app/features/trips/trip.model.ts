0d2def3e0b1ba88d316bc121c1e9cf6a3875e958
dangalcan <megamagolas@gmail.com>
2026-03-24 16:03:40 +0100

    fix: fix regexp of ticker in trip model

--- src/app/features/trips/trip.model.ts
+++ src/app/features/trips/trip.model.ts
@@ -3,7 +3,7 @@ import { Entity } from "../../shared/entity.model";
 export type DifficultyLevel = 'EASY' | 'MEDIUM' | 'HARD';
 
 export const TRIP_VALIDATION = {
-    ticker:          { pattern: /^\d{6}-[A-Z]{4}$/ },
+    ticker:          { pattern: /^\d{2}(?:(?:0[13578]|1[02])(0[1-9]|[12][0-9]|3[01])|(?:0[469]|11)(0[1-9]|[12][0-9]|30)|02(0[1-9]|1[0-9]|2[0-9]))-[A-Z]{4}$/ },
     title:           { maxLength: 100 },
     description:     { maxLength: 1000 },
     maxParticipants: { min: 1 },
