20db466663e818dd40cb8ffa53ad47a7822c777b
Jaime Caballero <jcaballeroh02@gmail.com>
2026-03-25 18:39:42 +0100

    feat: enhance trip card and footer components with new pipes for improved data formatting

--- src/app/shared/layout/footer/footer.component.ts
+++ src/app/shared/layout/footer/footer.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterPipe } from '../../pipes/footer.pipe';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [FooterPipe],
   templateUrl: './footer.component.html',
   // styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  readonly companyName = 'Acme Explorer, Inc';
+}
