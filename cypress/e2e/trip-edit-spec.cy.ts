b5c671f6a3eb9f6c17e1756991dbadd63ca6fa1d
dangalcan <megamagolas@gmail.com>
2026-05-01 09:54:40 +0200

    test: add e2e trip edition tests

---
+++ cypress/e2e/trip-edit-spec.cy.ts
@@ -0,0 +1,144 @@
+import { LoginPage } from '../page-objects/login.po';
+import { HeaderPage } from '../page-objects/header.po';
+import { TripDisplayPage } from '../page-objects/trip_display.po';
+import { TripEditPage } from '../page-objects/trip-edit.po';
+import { TripCreatePage } from '../page-objects/trip-create.po';
+
+describe('Trip edition', () => {
+    const loginPage = new LoginPage();
+    const headerPage = new HeaderPage();
+    const tripDisplayPage = new TripDisplayPage();
+    const tripEditPage = new TripEditPage();
+    const tripCreatePage = new TripCreatePage();
+
+    let tripId: string;
+
+    const clearStorage = () => {
+        cy.clearAllCookies();
+        cy.clearAllLocalStorage();
+        cy.clearAllSessionStorage();
+        cy.window().then((win) => {
+            return win.indexedDB.databases().then((dbs) => {
+                dbs.forEach((db) => win.indexedDB.deleteDatabase(db.name!));
+            });
+        });
+    };
+
+    const loginAsManager = () => {
+        loginPage.navigateTo();
+        loginPage.fillInLoginFormAsManager();
+        headerPage.getLogoutButton().should('be.visible');
+    };
+
+    before(() => {
+        clearStorage();
+        loginAsManager();
+        tripCreatePage.navigateTo();
+        tripCreatePage.fillRequiredFieldsAndSubmit({
+            title: 'E2E Edit Test Trip',
+            description: 'Created by Cypress for trip edition tests',
+            startDate: '2028-06-01',
+            endDate: '2028-07-01',
+            stageTitle: 'Stage One',
+            stageDescription: 'Stage One description',
+            stagePrice: '100',
+        });
+        cy.url()
+            .should('not.include', '/trips/create')
+            .should('match', /\/trips\/[^/]+$/)
+            .then((url) => {
+                tripId = url.split('/trips/')[1];
+            });
+    });
+
+    after(() => {
+        clearStorage();
+        loginAsManager();
+        cy.then(() => cy.visit(`localhost:4200/trips/${tripId}`));
+        tripDisplayPage.getDeleteButton().click();
+        cy.url().should('match', /\/trips$/);
+    });
+
+    beforeEach(() => {
+        // Intercept Firestore calls to help stabilize the test
+        cy.intercept('GET', '**/google.firestore.v1.Firestore/Listen/**').as('firestoreListen');
+        
+        clearStorage();
+        loginAsManager();
+        tripEditPage.navigateTo(tripId);
+        
+        cy.url().should('include', '/edit');
+        
+        // Wait for the form to populate. We use a longer timeout here because 
+        // Firestore cold starts can be slow.
+        tripEditPage.getTitleInput().should('not.have.value', '', { timeout: 10000 });
+        
+        // Brief pause to allow Angular's reactive form to stabilize after ngOnChanges
+        cy.wait(500); 
+    });
+
+    it('should display edit form pre-filled with trip data', () => {
+        tripEditPage.getTitleInput().should('not.have.value', '');
+        tripEditPage.getDescriptionInput().should('not.have.value', '');
+        tripEditPage.getSubmitButton().should('not.be.disabled');
+    });
+
+    it('should successfully save edit and redirect to trip display', () => {
+        const newTitle = 'Updated E2E Title ' + Date.now();
+
+        // FIX: Using {selectall}{backspace} ensures the field is empty even if 
+        // Angular tries to re-pipe data into the input mid-type.
+        tripEditPage.getTitleInput()
+            .click()
+            .type('{selectall}{backspace}')
+            .type(newTitle)
+            .should('have.value', newTitle);
+
+        tripEditPage.submit();
+
+        // Ensure we left the edit page
+        cy.url().should('not.include', '/edit');
+        cy.url().should('include', `/trips/${tripId}`);
+
+        // Force a reload to clear any local cache and fetch fresh from Firestore
+        cy.reload();
+
+        // FIX: Use 'have.text' for an exact match to prevent "Frankenstein" titles
+        cy.get('h1', { timeout: 10000 }).should('have.text', newTitle);
+    });
+
+    it('should cancel edit and return to trip display page', () => {
+        tripEditPage.cancel();
+        cy.url().should('not.include', '/edit');
+        cy.url().should('include', '/trips/');
+    });
+
+    it('should show validation error when title is blank', () => {
+        tripEditPage.getTitleInput().click().type('{selectall}{backspace}');
+        tripEditPage.submit();
+        tripEditPage.getTitleInput().should('have.class', 'border-red-500');
+        cy.url().should('include', '/edit');
+    });
+
+    it('should show validation error for past start date', () => {
+        tripEditPage.getStartDateInput().clear().type('2020-01-01');
+        tripEditPage.submit();
+        tripEditPage.getStartDateInput().should('have.class', 'border-red-500');
+        cy.url().should('include', '/edit');
+    });
+
+    it('should show validation error when end date is before start date', () => {
+        tripEditPage.getStartDateInput().clear().type('2030-06-01');
+        tripEditPage.getEndDateInput().clear().type('2030-05-01');
+        tripEditPage.submit();
+        tripEditPage.getEndDateInput().should('have.class', 'border-red-500');
+        cy.url().should('include', '/edit');
+    });
+
+    it('should successfully change difficulty level', () => {
+        tripEditPage.getDifficultySelect().select('HARD');
+        tripEditPage.submit();
+        cy.url().should('not.include', '/edit');
+        cy.url().should('include', '/trips/');
+    });
+});
